273bf55e341ffe313c45d157fdaf0cb00e865c6d
Christopher Fraser <cfraz89@gmail.com>
2019-12-27 09:39:33 +0800

    Nice type errors when capabilities checks fail

--- src/Data/Tapioca/Internal/Types/Capability.hs
+++ src/Data/Tapioca/Internal/Types/Capability.hs
@@ -5,6 +5,7 @@
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE PolyKinds #-}
 {-# LANGUAGE UndecidableInstances #-}
+{-# LANGUAGE ConstraintKinds #-}
 
 module Data.Tapioca.Internal.Types.Capability 
   ( Capability(..)
@@ -15,8 +16,10 @@ module Data.Tapioca.Internal.Types.Capability
   , Can
   ) where
 
+import Data.Constraint
 import Data.Type.Bool
 import Data.Type.Equality
+import GHC.TypeLits
 
 data Capability = Encode | Decode
 
@@ -26,9 +29,14 @@ type Encode = Only 'Encode
 type Decode = Only 'Decode
 type EncodeDecode = ['Encode, 'Decode]
 
-type family Can' c cs where
-  Can' c '[] = 'False
-  Can' c (c' ': cs') = c == c' || Can' c cs'
-
 class Can c cs
-instance Can' c cs ~ 'True => Can c cs
+
+type family Has c cs where
+  Has c '[] = 'False
+  Has c (c' ': cs') = c == c' || Has c cs'
+
+type family EnsureCan c cs t :: Constraint where
+  EnsureCan c cs 'True = ()
+  EnsureCan c cs 'False = TypeError ('Text "No capability " ':<>: 'ShowType c ':<>: 'Text " in " ':<>: 'ShowType cs)
+
+instance EnsureCan c cs (Has c cs) => Can c cs
